508dc1a61c82cf81459f51bde6042938861c4126
f3 <f3kilo@yandex.ru>
2023-11-26 17:56:37 +0300

    ledger types + node initialization

---
+++ ledger-types/src/lib.rs
@@ -0,0 +1,211 @@
+use std::net::SocketAddr;
+
+use k256::ecdsa::{RecoveryId, Signature as K256Signature, SigningKey, VerifyingKey};
+use k256::elliptic_curve::consts::U32;
+use k256::elliptic_curve::generic_array::GenericArray;
+use k256::schnorr::signature::hazmat::PrehashSigner;
+use k256::sha2::Digest;
+
+#[derive(Debug)]
+pub struct BlockData {
+    pub prev_hash: B256,
+    pub number: u64,
+    pub transactions: Vec<Transaction>,
+}
+
+impl BlockData {
+    pub fn hash(&self) -> B256 {
+        let mut hasher = k256::sha2::Sha256::new();
+        hasher.update(&self.prev_hash.0);
+
+        for tx in &self.transactions {
+            hasher.update(&tx.hash.0);
+        }
+
+        let result = hasher.finalize();
+        B256(result.into())
+    }
+}
+
+#[derive(Debug)]
+pub struct Block {
+    pub hash: B256,
+    pub data: BlockData,
+    pub proposer: B256,
+    pub signature: Signature,
+}
+
+impl Block {
+    /// Creates a new signed block.
+    pub fn new(data: BlockData, signer: &SigningKey) -> Self {
+        let hash = data.hash();
+        let signature = Signature::sign(signer, hash);
+        let proposer = B256::address_of(signer.verifying_key());
+
+        Self {
+            hash,
+            data,
+            proposer,
+            signature,
+        }
+    }
+
+    /// Check correctness of block signature.
+    pub fn verify(&self) -> Option<()> {
+        let expected_hash = self.data.hash();
+        if self.hash != expected_hash {
+            return None;
+        }
+
+        let expectet_proposer = self.signature.recover(expected_hash)?;
+        if self.proposer != expectet_proposer {
+            return None;
+        }
+
+        Some(())
+    }
+}
+
+#[derive(Debug)]
+pub struct TransactionData {
+    pub to: B256,
+    pub amount: u64,
+    pub nonce: u64,
+}
+
+impl TransactionData {
+    pub fn hash(&self) -> B256 {
+        let mut hasher = k256::sha2::Sha256::new();
+        hasher.update(&self.to.0);
+        hasher.update(&self.amount.to_be_bytes());
+        hasher.update(&self.nonce.to_be_bytes());
+        let result = hasher.finalize();
+        B256(result.into())
+    }
+}
+
+#[derive(Debug)]
+pub struct Transaction {
+    pub hash: B256,
+    pub from: B256,
+    pub data: TransactionData,
+    pub signature: Signature,
+}
+
+impl Transaction {
+    pub fn new(data: TransactionData, signer: &SigningKey) -> Self {
+        let hash = data.hash();
+        let signature = Signature::sign(signer, hash);
+        let from = B256::address_of(signer.verifying_key());
+        Self {
+            hash,
+            from,
+            data,
+            signature,
+        }
+    }
+
+    /// Check correctness of transaction signature.
+    pub fn verify(&self) -> Option<()> {
+        let expected_hash = self.data.hash();
+        if self.hash != expected_hash {
+            return None;
+        }
+
+        let expected_from = self.signature.recover(expected_hash)?;
+        if self.from != expected_from {
+            return None;
+        }
+
+        Some(())
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct B256(pub [u8; 32]);
+
+impl B256 {
+    pub fn hash_of(data: impl AsRef<[u8]>) -> Self {
+        let mut hasher = k256::sha2::Sha256::new();
+        hasher.update(data);
+        let result = hasher.finalize();
+        Self(result.into())
+    }
+
+    pub fn address_of(key: &VerifyingKey) -> Self {
+        let encoded_point = &key.to_encoded_point(false);
+        let data = encoded_point.as_bytes();
+        Self::hash_of(data)
+    }
+}
+
+
+pub struct NodeInfo {
+    pub name: String,
+    pub address: B256,
+    pub socket: SocketAddr,
+}
+
+#[derive(Debug)]
+pub struct Signature {
+    pub r: B256,
+    pub s: B256,
+    pub recovery_id: u8,
+}
+impl Signature {
+    fn sign(signer: &SigningKey, hash: B256) -> Self {
+        let (sig, ver) = signer
+            .sign_prehash(&hash.0)
+            .expect("prehash should be signed");
+
+        Self {
+            r: B256(sig.r().to_bytes().try_into().unwrap()),
+            s: B256(sig.s().to_bytes().try_into().unwrap()),
+            recovery_id: ver.to_byte(),
+        }
+    }
+
+    pub fn recover(&self, hash: B256) -> Option<B256> {
+        let (recoverable_sig, recovery_id) = self.as_signature();
+        let verify_key =
+            VerifyingKey::recover_from_prehash(&hash.0, &recoverable_sig, recovery_id).ok()?;
+
+        let address = B256::address_of(&verify_key);
+        Some(address)
+    }
+
+    /// Checks if the signature is created by the `address`.
+    pub fn verify(&self, hash: B256, address: B256) -> Option<()> {
+        let recovered = self.recover(hash)?;
+        (recovered == address).then_some(())
+    }
+
+    /// Retrieves the recovery signature.
+    fn as_signature(&self) -> (K256Signature, RecoveryId) {
+        let recovery_id = RecoveryId::from_byte(self.recovery_id).unwrap();
+        let r: &GenericArray<u8, U32> = GenericArray::from_slice(&self.r.0);
+        let s: &GenericArray<u8, U32> = GenericArray::from_slice(&self.s.0);
+        let sig = K256Signature::from_scalars(*r, *s).unwrap();
+        (sig, recovery_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use k256::ecdsa::SigningKey;
+
+    use crate::{Signature, B256};
+
+    #[test]
+    fn sign_and_verify() {
+        let signer = SigningKey::from_slice(&[42; 32]).unwrap();
+
+        let hash = B256::default();
+        let signature = Signature::sign(&signer, hash);
+        signature
+            .verify(hash, B256::address_of(signer.verifying_key()))
+            .unwrap();
+
+        assert!(signature.verify(hash, B256::default()).is_none());
+    }
+}
--- node/src/main.rs
+++ node/src/main.rs
@@ -1,3 +1,64 @@
+use std::collections::{BTreeMap, HashMap};
+use std::net::SocketAddr;
+
+use clap::{arg, Parser};
+use k256::ecdsa::SigningKey;
+use ledger_types::{Block, NodeInfo, Transaction, B256};
+
+#[derive(Debug, Parser)]
+struct Params {
+    #[arg(short, long)]
+    name: Option<String>,
+
+    #[clap(short, long)]
+    socket: SocketAddr,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let params = Params::parse();
+
+    let name = params
+        .name
+        .unwrap_or_else(|| names::Generator::default().next().unwrap());
+
+    let signer = SigningKey::random(&mut rand::thread_rng());
+    let address = B256::address_of(signer.verifying_key());
+
+    let node_info = NodeInfo {
+        name,
+        address,
+        socket: params.socket,
+    };
+
+    let node = Node::new(signer, node_info);
+}
+
+struct Node {
+    info: NodeInfo,
+    signer: SigningKey,
+    others: BTreeMap<B256, NodeInfo>,
+    blocks: Blocks,
+    pending_transactions: Vec<Transaction>,
+}
+
+impl Node {
+    fn new(signer: SigningKey, info: NodeInfo) -> Self {
+        let others = BTreeMap::new();
+        let blocks = Blocks::default();
+        let pending_transactions = Vec::new();
+
+        Self {
+            info,
+            signer,
+            others,
+            blocks,
+            pending_transactions,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Blocks {
+    hashes: Vec<B256>,
+    data: HashMap<B256, Block>,
 }
